70ad118a6e3eb2629b88f7ee4c645557cd6798c3
Xspence06 <68206139+Xspence06@users.noreply.github.com>
2020-07-24 17:10:51 -0500

    Add files via upload

---
+++ Final_Project.py
@@ -0,0 +1,9 @@
+gMajChords = "Gm Am Bm Cmaj Dmaj Em Fdim "
+cMajChords = "Cmaj Dm Em Fmaj Gmaj Am Bdim "
+dMajChords = "Dmaj Em F#m Gmaj Amaj Bmin C#dim"
+eMajChords = "Emaj F#m G#min Amaj Bmaj C#m D#dim"
+
+def chord_selector():
+    return print("Here are your chords?")
+print(cMajChords)
+chord_selector() 
