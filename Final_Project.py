9f67c729bbae5d295ca9e0555b1ce0cb999dd258
Xspence06 <68206139+Xspence06@users.noreply.github.com>
2020-07-25 17:58:22 -0500

    Add files via upload

--- Final_Project.py
+++ Final_Project.py
@@ -1,5 +1,13 @@
+import random
+
 gMajChords = ["Gm", "Am","Bm","Cmaj","Dmaj","Em","Fdim"]
 cMajChords = ["Cmaj", "Dm", "Em", "Fmaj", "Gmaj", "Am", "Bdim"]
 dMajChords = ["Dmaj", "Em", "F#m", "Gmaj", "Amaj", "Bmin", "C#dim"]
 eMajChords = ["Emaj", "F#m", "G#min", "Amaj", "Bmaj", "C#m", "D#dim"]
-print(gMajChords[1], print(gMajChords[2], print(gMajChords[5])))
+print ("Choose a random Chord from the list")
+sampled_list = random.sample(eMajChords,4)
+print(sampled_list)
+print("Here are your chords!")
+
+#This program will give you a random chord progression from the key of your choosing.
+#Select one of the MajChords, and select up 3-6 chords that you want in the progession.
