8ecf661b33c698eb714b91eb2cc88949ad5f130a
Xspence06 <68206139+Xspence06@users.noreply.github.com>
2020-07-24 20:47:13 -0500

    Add files via upload

--- Final_Project.py
+++ Final_Project.py
@@ -1,9 +1,5 @@
-gMajChords = "Gm Am Bm Cmaj Dmaj Em Fdim "
-cMajChords = "Cmaj Dm Em Fmaj Gmaj Am Bdim "
-dMajChords = "Dmaj Em F#m Gmaj Amaj Bmin C#dim"
-eMajChords = "Emaj F#m G#min Amaj Bmaj C#m D#dim"
-
-def chord_selector():
-    return print("Here are your chords?")
-print(cMajChords)
-chord_selector() 
+gMajChords = ["Gm", "Am","Bm","Cmaj","Dmaj","Em","Fdim"]
+cMajChords = ["Cmaj", "Dm", "Em", "Fmaj", "Gmaj", "Am", "Bdim"]
+dMajChords = ["Dmaj", "Em", "F#m", "Gmaj", "Amaj", "Bmin", "C#dim"]
+eMajChords = ["Emaj", "F#m", "G#min", "Amaj", "Bmaj", "C#m", "D#dim"]
+print(gMajChords[1], print(gMajChords[2], print(gMajChords[5])))
